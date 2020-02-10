428a106a73b7ba6a50e8d66aab4311ca26793f10
Wesley Araujo <wparaujo@gmail.com>
2020-02-10 16:25:57 -0300

    add push and pull

--- lib/mylib.rb
+++ lib/mylib.rb
@@ -12,11 +12,19 @@ class MyLib < Thor
         end
     end
     
-    # desc 'get status of all libs', 'get git status for all development libs'
-    # def pull 
-    # end
+    desc 'get status of all libs', 'get git status for all development libs'
+    def pull 
+        Open3.popen3('find . -type d -name .git -exec git --git-dir={} --work-tree=$PWD/{}/.. pull \;') do | stdin, stdout, stderr, wait_thr|
+            puts stdout.read
+            # puts stderr.read
+        end
+    end
 
-    # desc 'get status of all libs', 'get git status for all development libs'
-    # def push
-    # end
+    desc 'get status of all libs', 'get git status for all development libs'
+    def push
+        Open3.popen3('find . -type d -name .git -exec git --git-dir={} --work-tree=$PWD/{}/.. push \;') do | stdin, stdout, stderr, wait_thr|
+            puts stdout.read
+            # puts stderr.read
+        end
+    end
 end
